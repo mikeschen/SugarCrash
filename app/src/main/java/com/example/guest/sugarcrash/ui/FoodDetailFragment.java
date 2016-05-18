4640fbcb5afaae68ad6c86eee10438978149044d
Daren Schaad and Mike Schennum <mikeschenphoto@gmail.com>
2016-05-18 08:55:10 -0700

    Create new model for saved food

--- app/src/main/java/com/example/guest/sugarcrash/ui/FoodDetailFragment.java
+++ app/src/main/java/com/example/guest/sugarcrash/ui/FoodDetailFragment.java
@@ -2,7 +2,7 @@ package com.example.guest.sugarcrash.ui;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
